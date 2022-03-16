af0ef0817e504b7d5b00ddb664407899d85c9ff1
ldimbe20 <ldimbe20@gmail.com>
2022-03-16 14:11:42 -0500

    Created mediums are now being shown

--- src/components/mediums/MediumForm.js
+++ src/components/mediums/MediumForm.js
@@ -5,13 +5,18 @@ import { useHistory } from 'react-router-dom'
 
 export const MediumForm = ({ setMediums, mediums }) => {
     const name = useRef(null)
+    const history = useHistory()
 
     const newMedium = (evt) => {
         evt.preventDefault()
-            createMedium({
+            return createMedium({
                 name: name.current.value
             })
-                .then(setMediums)
+                .then(data => {
+                    setMediums(data)
+                    return true
+                })
+                
         }
     
 
@@ -30,11 +35,32 @@ export const MediumForm = ({ setMediums, mediums }) => {
                     <input className="box" type="text" id="mediumName" ref={name} required autoFocus placeholder="Add medium name" />
                 </div>
             </fieldset>
-            <button className="button mt-2 m-1" type="submit" onClick={(evt) => { newMedium(evt) }}>
+            {/* <button className="button mt-2 m-1" type="submit"
+             onClick={(evt) => { newMedium(evt).then(setMediums).then(() => history.push('/mediums'))}}>
+                Save Medium
+            </button> */}
+            <button className="button mt-2 m-1" type="submit"
+             onClick={(evt) => { newMedium(evt)
+             .then(() => history.push('/mediums'))}}>
                 Save Medium
             </button>
+
         </form>
 
     )
 }
+
+
+// onClick={(evt) => { newMedium(evt) }}>
+
+{/* <button
+													className='button is-link is-dark'
+													onClick={() => {
+														deletePost(
+															finishedPost.id
+														).then(getPosts).then((data) => setPosts(data))
+														.then(() => history.push('/private_posts'))
+													}}>
+													Delete
+												</button> */}
 
