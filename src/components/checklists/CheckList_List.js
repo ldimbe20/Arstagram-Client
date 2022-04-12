0ccf88c8dcc4366d82782cc22d424ffda1faeed7
ldimbe20 <ldimbe20@gmail.com>
2022-04-12 10:10:46 +0700

    Created form to create checkbox

--- src/components/checklists/CheckList_List.js
+++ src/components/checklists/CheckList_List.js
@@ -5,7 +5,8 @@
 
 import { useEffect, useState } from "react"
 import { getChecklists } from "./ChecklistManager"
-import { ShowChecklist } from "./Checklist"
+import { ShowChecklist} from "./Checklist"
+import { ChecklistForm} from "./ChecklistForm"
 import "./checklist.css"
 
 
@@ -25,13 +26,18 @@ export const Checklist = () => {
                     <h1 className="main-title">Checklist</h1>
                     <p class="mb-4">Add a checklist of art tasks you would like to complete. </p>
                     <h2 className="title is-5">Your Current Checklist</h2> 
-                                <button className="button is-primary">Add CheckList Item</button>
+                                <button className="button is-primary">
+                                         Add CheckList Item</button>
                                 {/* import checklist form functions-set props to be used*/}
+
+                               
+                                
        
                                 <div className = "p-4columns mt-5 is-8 is-variable">
                                         
                                         <ShowChecklist setChecklists={setChecklists} checklists={checklists}  /> 
                                 </div>
+
                                 
                 
                 </div>
